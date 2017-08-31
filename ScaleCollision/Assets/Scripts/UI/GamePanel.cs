641a91814f9671a18a887f146a4d0d5878ccc0d8
Jie <14wjwu@stu.edu.cn>
2017-08-31 14:50:24 +0800

    实现游戏界面中。。。

---
+++ ScaleCollision/Assets/Scripts/UI/GamePanel.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GamePanel : MonoBehaviour 
+{
+
+	private Button JumpButton;
+	private Button SkillButton;
+
+    private List<int> ScaleIdList = new List<int>();
+    private List<Sprite> ScaleSprite = new List<Sprite>();
+	private List<UIScale> ScaleList = new List<UIScale>();
+
+	// Use this for initialization
+	void Awake () 
+	{
+		OnCreate ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void OnCreate()
+	{
+		JumpButton = GameObject.Find("JumpButton").GetComponent<Button>();
+        InitialScale();
+		JumpButton.onClick.AddListener (()=>{
+            ScaleIdList = UIScale.RandomScale(ScaleIdList);
+            for(int i=0; i<16; i++){
+                ScaleList[i].RefreshUI(ScaleIdList[i], ScaleSprite[ScaleIdList[i]]);
+                Debug.Log(ScaleIdList[i] + " ");
+            }
+
+		});
+	}
+
+    public void InitialScale()
+    {
+        string path1 = "Images/UI/GamePanel/left{0}";
+        string path2 = "Images/UI/GamePanel/right{0}";
+        for (int i = 0; i < 8; i++)
+        {
+            Sprite tempSprite = Resources.Load(string.Format(path1, i), typeof(Sprite)) as Sprite;
+            ScaleSprite.Add(tempSprite);
+        }
+        for (int i = 0; i < 8; i++)
+        {
+            Sprite tempSprite = Resources.Load(string.Format(path2, i), typeof(Sprite)) as Sprite;
+            ScaleSprite.Add(tempSprite);
+        }
+
+        for (int i = 0; i < 16; i++) 
+        {
+            ScaleIdList.Add(i);
+            ScaleList.Add (new UIScale (i));
+        }
+    }
+
+}
--- ScaleCollision/Assets/Scripts/UICtrl/UIScale.cs
+++ ScaleCollision/Assets/Scripts/UICtrl/UIScale.cs
@@ -1,15 +1,94 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
+//this class is to control UIScale.
 public class UIScale : MonoBehaviour {
+	public int id;
+	public int ScaleId{ get; set;}
 
-	// Use this for initialization
-	void Start () {
-	
+    private string LeftScaleName = "Scale/LeftScale/LeftScale{0}";
+    private string RightScaleName = "Scale/RightScale/RightScale{0}";
+    private static readonly int oneSideNum = 8;
+    private static readonly int oneScaleLength = 80;
+
+	public Image mScale;
+
+	public UIScale(int id)
+	{
+		this.id = id;
+        SetUI();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void SetUI()
+	{
+        if (id < oneSideNum) 
+        {
+            mScale = GameObject.Find(string.Format(LeftScaleName, id)).GetComponent<Image>();
+        } else 
+        {
+            mScale = GameObject.Find (string.Format (RightScaleName, id-oneSideNum)).GetComponent<Image> ();
+        }
+        ScaleId = id;
 	}
+
+    public void RefreshUI(int ScaleId, Sprite sprite)
+	{
+        this.ScaleId = ScaleId;
+        mScale.sprite = sprite;
+	}
+        
+
+    //when player is jumping, getting which scale he has jumped.
+    public static int GetScalePositionId(Vector3 playerPosition)
+    {
+        float positionX = playerPosition.x;
+        int ScalePositionId = 0;
+        if (positionX < 0)
+        {
+            float absPositionX = System.Math.Abs(positionX);
+            ScalePositionId = (int)(oneSideNum - absPositionX / oneScaleLength - 1);
+        }
+        else
+        {
+            ScalePositionId = (int)(2 * oneSideNum - positionX / oneScaleLength - 1);
+        }
+        return ScalePositionId;
+    }
+
+    //Random Scales Sequence.
+    public static List<int> RandomScale(List<int> ScaleIdList)
+    {
+        int length = ScaleIdList.Count / 2;
+        int[] tempList = new int[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            tempList[i] = ScaleIdList[i];
+        }
+
+        //range pick 1-length, to avoid changing the white cloud(防止改变了两端的缓冲地带).
+        for (int i = 0; i < 100; i++)
+        {
+            int randomIndex1 = Random.Range(1, length);
+            int randomIndex2 = Random.Range(1, length);
+            int temp = tempList[randomIndex1];
+            tempList[randomIndex1] = tempList[randomIndex2];
+            tempList[randomIndex2] = temp;
+        }
+
+        List<int> newScaleIdList = new List<int>();
+
+        for (int i = 0; i < length; i++)
+        {
+            newScaleIdList.Add(tempList[i]);
+            Debug.Log("Sequenece" + tempList[i]);
+        }
+        for (int i = 0; i < length; i++)
+        {
+            newScaleIdList.Add(tempList[i]+oneSideNum);   
+        }
+        return newScaleIdList;
+    }
 }
